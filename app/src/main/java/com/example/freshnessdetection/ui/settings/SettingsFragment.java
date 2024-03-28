094a9103decbb2258dc44ccbe5239ed57a91f405
Kayuemkhan <abdulkayuem007@gmail.com>
2024-03-28 16:01:42 +0600

    about page added

--- app/src/main/java/com/example/freshnessdetection/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/example/freshnessdetection/ui/settings/SettingsFragment.java
@@ -1,5 +1,6 @@
 package com.example.freshnessdetection.ui.settings;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +10,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import com.example.freshnessdetection.databinding.FragmentSettingsBinding;
+import com.example.freshnessdetection.ui.about.AboutActivity;
 
 public class SettingsFragment extends Fragment {
 
@@ -22,6 +24,8 @@ public class SettingsFragment extends Fragment {
     binding = FragmentSettingsBinding.inflate(inflater, container, false);
     View root = binding.getRoot();
 
+    binding.aboutUs.getRoot().setOnClickListener(
+        view -> startActivity(new Intent(requireActivity(), AboutActivity.class)));
     return root;
   }
 
