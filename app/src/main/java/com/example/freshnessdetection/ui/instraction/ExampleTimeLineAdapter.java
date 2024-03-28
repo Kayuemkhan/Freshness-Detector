e87c5c256f6660798df3f94dfdd1a56993a76d69
Kayuemkhan <abdulkayuem007@gmail.com>
2024-03-29 02:40:10 +0600

    instraction timeline added

---
+++ app/src/main/java/com/example/freshnessdetection/ui/instraction/ExampleTimeLineAdapter.java
@@ -0,0 +1,80 @@
+package com.example.freshnessdetection.ui.instraction;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.freshnessdetection.R;
+import com.example.freshnessdetection.ui.instraction.model.TimeLineModel;
+import com.example.freshnessdetection.util.VectorDrawableUtils;
+import com.github.vipulasri.timelineview.TimelineView;
+
+import java.util.List;
+
+public class ExampleTimeLineAdapter extends RecyclerView.Adapter<ExampleTimeLineAdapter.TimeLineViewHolder> {
+
+    private final List<TimeLineModel> mFeedList;
+
+    public ExampleTimeLineAdapter(List<TimeLineModel> feedList) {
+        mFeedList = feedList;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return TimelineView.getTimeLineViewType(position, getItemCount());
+    }
+
+    @NonNull
+    @Override
+    public TimeLineViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View itemView = inflater.inflate(R.layout.item_timeline, parent, false);
+        return new TimeLineViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TimeLineViewHolder holder, int position) {
+        TimeLineModel timeLineModel = mFeedList.get(position);
+
+        if ("INACTIVE".equals(timeLineModel.getStatus())) {
+            setMarker(holder, R.drawable.ic_marker_inactive, R.color.grey);
+        } else if ("ACTIVE".equals(timeLineModel.getStatus())) {
+            setMarker(holder, R.drawable.ic_marker_active, R.color.grey);
+        } else {
+            setMarker(holder, R.drawable.ic_marker, R.color.grey);
+        }
+
+        holder.text_timeline_date.setText(timeLineModel.getDate());
+        holder.text_timeline_title.setText(timeLineModel.getMessage());
+    }
+
+    private void setMarker(TimeLineViewHolder holder, int drawableResId, int colorResId) {
+        Context context = holder.itemView.getContext();
+        holder.timeline.setMarker(VectorDrawableUtils.getDrawable(context, drawableResId, ContextCompat.getColor(context, colorResId)));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mFeedList.size();
+    }
+
+    static class TimeLineViewHolder extends RecyclerView.ViewHolder {
+        TimelineView timeline;
+        TextView text_timeline_date;
+        TextView text_timeline_title;
+
+        TimeLineViewHolder(View itemView) {
+            super(itemView);
+            timeline = itemView.findViewById(R.id.timeline);
+            text_timeline_date = itemView.findViewById(R.id.text_timeline_date);
+            text_timeline_title = itemView.findViewById(R.id.text_timeline_title);
+        }
+    }
+}
--- app/src/main/java/com/example/freshnessdetection/ui/instraction/InstractionFragment.java
+++ app/src/main/java/com/example/freshnessdetection/ui/instraction/InstractionFragment.java
@@ -3,27 +3,50 @@ package com.example.freshnessdetection.ui.instraction;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.freshnessdetection.R;
+import com.example.freshnessdetection.ui.instraction.model.TimeLineModel;
+
+import java.util.ArrayList;
 
 public class InstractionFragment extends Fragment {
+    private final ArrayList<TimeLineModel> mDataList  = new ArrayList<>();
 
-
+    private RecyclerView recyclerView;
     public InstractionFragment() {
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setDataListItems();
+        initRecyclerView();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_instraction, container, false);
+        View view = inflater.inflate(R.layout.fragment_instraction, container, false);
+        recyclerView = view.findViewById(R.id.recyclerViews);
+        return view;
+    }
+    private void setDataListItems() {
+        mDataList.add(new TimeLineModel("Item successfully delivered", "", "INACTIVE"));
+        mDataList.add(new TimeLineModel("Courier is out to delivery your order", "2017-02-12 08:00", "ACTIVE"));
+        mDataList.add(new TimeLineModel("Order placed successfully", "2017-02-10 14:00", "COMPLETED"));
+    }
+
+    private void initRecyclerView() {
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(requireActivity(), RecyclerView.VERTICAL, false);
+        recyclerView.setLayoutManager(mLayoutManager);
+        ExampleTimeLineAdapter exampleTimeLineAdapter = new ExampleTimeLineAdapter(mDataList);
+        recyclerView.setAdapter(exampleTimeLineAdapter);
     }
 }
